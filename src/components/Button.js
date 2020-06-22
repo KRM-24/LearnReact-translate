e519663d79d1e2ef71ecb56a69bb83543a699746
KRM-24 <36944142+KRM-24@users.noreply.github.com>
2020-06-22 15:32:03 -0500

    Pulling from Multiple Contexts

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
  
 class Button extends React.Component {
   renderSubmit(value) {
     return value === 'english' ? 'Submit' : 'Voorleggen';
   }
   
+
+
   render() {
     return (
-      <button className="ui button primary">
-        <LanguageContext.Consumer>
-          {(value) => this.renderSubmit(value)}
-        </LanguageContext.Consumer>
-      </button>
+      // only argument of a Consumer is a function...
+      <ColorContext.Consumer>
+        {(color) => 
+          <button className={`ui button ${color}`}>
+          <LanguageContext.Consumer>
+            {(value) => this.renderSubmit(value)}
+          </LanguageContext.Consumer>
+        </button>
+        }
+      </ColorContext.Consumer>
     );
   }
 }
